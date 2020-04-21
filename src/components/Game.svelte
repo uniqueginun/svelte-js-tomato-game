0f4b1b42ead1b319c65eeb6e0344d8f9f8f7dec1
Azzoz AlHasani <AHasani@makkah.gov.sa>
2020-04-21 11:49:41 +0300

    add difficulty level

--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -4,9 +4,8 @@
   let n = 32;
   $: gameArray = Array(n);
   let activeIndex = null;
-  let activeInterval = setInterval(() => {
-    activeIndex = Math.floor(Math.random() * Math.floor(n - 1));
-  }, 1000);
+  let start = false;
+  let level = 1000;
 
   const handleSelect = ({ detail }) => {
     if (n === 1) {
@@ -20,6 +19,13 @@
     }
 
     attempts = attempts + 1;
+  };
+
+  const startGame = () => {
+    let activeInterval = setInterval(() => {
+      activeIndex = Math.floor(Math.random() * Math.floor(n - 1));
+    }, level);
+    start = true;
   };
 </script>
 
@@ -35,6 +41,9 @@
     width: 700px;
     height: 1024px;
   }
+  .game-title {
+    font-size: 40px;
+  }
   .game {
     text-align: center;
   }
@@ -43,7 +52,7 @@
     text-align: center;
     color: tomato;
     font-weight: bolder;
-    font-size: 40px;
+    font-size: 30px;
   }
   .game-body {
     display: flex;
@@ -51,19 +60,45 @@
     flex: 1;
     padding: 30px 50px;
   }
+  .diff {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+  }
 </style>
 
 <main class="container">
   <div class="game">
-    <h2 class="game-title">Game Svelte</h2>
+    <h2 class="game-title">Tomato Game</h2>
     <div class="attempts">
-      Attempts
+      failed attempts
       <span class="att-num">{attempts}</span>
     </div>
     <div class="game-body">
-      {#each gameArray as _, i}
-        <GameItem {activeIndex} on:select={handleSelect} index={i} />
-      {/each}
+      {#if !start}
+        <div class="diff">
+          <label>Select difficulty</label>
+          <br />
+          <label>
+            <input type="radio" value={1500} bind:group={level} />
+            Easy
+          </label>
+          <label>
+            <input type="radio" value={1000} bind:group={level} />
+            Medium
+          </label>
+          <label>
+            <input type="radio" value={500} bind:group={level} />
+            Hard
+          </label>
+          <button class="start-btn" on:click|once={startGame}>START</button>
+        </div>
+      {:else}
+        {#each gameArray as _, i}
+          <GameItem {activeIndex} on:select={handleSelect} index={i} />
+        {/each}
+      {/if}
+
     </div>
   </div>
 </main>
